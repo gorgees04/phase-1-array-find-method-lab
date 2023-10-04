0bce83505bf1401e92bde7e377aa316a27fbcc7b
Gorgees Odisho <gorgees041@gmail.com>
2023-10-05 02:44:38 +0300

    FIND METHOD DONE

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(record) {
+    const sadReality = record.find(win => win.result === "W")
+    if (sadReality){
+        return sadReality.year
+    }
+}
+
+
